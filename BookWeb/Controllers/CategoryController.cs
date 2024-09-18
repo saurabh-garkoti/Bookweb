8e1fe1aaa2662473a750f089ca9222f5d954dd27
Saurabh Garkoti <garkotisaurabhme@gmail.com>
2024-09-18 22:31:52 +0530

    Initial commit

---
+++ BookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,118 @@
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //var objCAtegoryList = _db.Categories.ToList();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+            //return View();
+        }
+        // get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomerError", "The DisplayOrder cannot excatly match the Name.");
+                ModelState.AddModelError("Name", "The DisplayOrder cannot excatly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category createed successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        // get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Retrieve id from database
+            var categoryFromDb = _db.Categories.Find(id);
+            //var CategoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //var CategoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null) 
+            { 
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //post 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomerError", "The DisplayOrder cannot excatly match the Name.");
+                ModelState.AddModelError("Name", "The DisplayOrder cannot excatly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Retrieve id from database
+            var categoryFromDb = _db.Categories.Find(id);
+            //var CategoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //var CategoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //post 
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int?id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+
+            TempData["Success"] = "Category Deleted successfully!";
+            return RedirectToAction("Index");   
+        }
+    }
+}
